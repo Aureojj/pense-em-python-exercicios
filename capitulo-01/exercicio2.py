13004e090e69c2f81ed0d75900663e627bb6b6c3
Jose Aureo <jose.sleite@pagar.me>
2023-02-17 14:37:36 -0300

    add: capitulo 2

--- capitulo-01/exercicio2.py
+++ capitulo-01/exercicio2.py
@@ -1,8 +1,10 @@
+# Exercícios realizados no modo interativo
+
 #1. Quantos segundos há em 42 minutos e 42 segundos? (sabendo que 1 minuto contém 60 segundos)
 
-minutos = 42
-segundos = 42
-totalDeSegundos = (42*60) + 42
+MINUTOS = 42
+SEGUNDOS = 42
+totalDeSegundos = (SEGUNDOS*MINUTOS) + 42
 print(totalDeSegundos, 'segundos')
 
 #R: 2562 segundos
@@ -11,9 +13,9 @@ print(totalDeSegundos, 'segundos')
 
 #2. Quantas milhas há em 10 quilômetros? Dica: uma milha equivale a 1,61 quilômetro.
 
-milha = 1.61
-quilometros = 10
-totalDeMilhas = quilometros/milha
+MILHA = 1.61
+QUILOMETROS = 10
+totalDeMilhas = QUILOMETROS/MILHA
 print(totalDeSegundos, 'milhas')
 #R: 6.211180124223602 milhas
 
@@ -21,18 +23,18 @@ print(totalDeSegundos, 'milhas')
 
 #3. Se você correr 10 quilômetros em 42 minutos e 42 segundos, qual é o seu passo médio (tempo por milha em minutos e segundos)? Qual é a sua velocidade média em milhas por hora?
 
-minutos = 42
-segundos = 0.42
-quilometros = 10
-milha = 1.61
+MINUTOS = 42
+SEGUNDOS = 0.42
+QUILOMETROS = 10
+MILHA = 1.61
 
-milhas = quilometros/milha
-totalDeMinutos = minutos+segundos
-passoMedio = (totalDeMinutos/milhas)
+MILHAS = QUILOMETROS/MILHA
+totalDeMinutos = MINUTOS+SEGUNDOS
+passoMedio = (totalDeMinutos/MILHAS)
 print('O passo médio é {:.2f}min/m'.format(passoMedio))
 # O passo médio é 6.83min/m
 
 horas = totalDeMinutos/60
-velocidadeMedia = milhas/horas
+velocidadeMedia = MILHAS/horas
 print('A velocidade média é {:.2f}m/h'.format(velocidadeMedia))
 # A velocidade média é 8.79m/h
